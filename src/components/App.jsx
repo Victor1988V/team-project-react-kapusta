e82d133414e851126462a77d43e636c4da8a1429
Kulik Victor <kulic02041988@gmail.com>
2023-01-26 19:57:14 +0200

    add routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
 export const App = () => {
