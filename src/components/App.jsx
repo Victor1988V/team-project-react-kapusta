d376b1e9fdeff2a5f6484808181be1d99e73d625
Kulik Victor <kulic02041988@gmail.com>
2023-01-26 19:49:32 +0200

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,44 @@
+import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+
 export const App = () => {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter basename="kapusta">
+        <ToastContainer />
+        <Routes>
+          <Route path="/" element={<div>SharedLayouts </div>}>
+            <Route path="/" element={<div>PrivateRoute </div>}>
+              <Route index element={<Navigate to="/home" />} />
+              {!isMobile && (
+                <>
+                  <Route path="/home" element={<div>HomePage</div>}>
+                    <Route index element={<Navigate to="/home/expenses" />} />
+                    <Route path="income" element={<div>IncomePage</div>} />
+                    <Route path="expenses" element={<div>ExpensesPage</div>} />
+                  </Route>
+                </>
+              )}
+              {isMobile && (
+                <>
+                  <Route path="/home" element={<div>HomePage</div>} />
+                  <Route path="/income" element={<div>IncomePage</div>} />
+                  <Route path="/expenses" element={<div>ExpensesPage</div>} />
+                  <Route path="*" element={<Navigate to="/home" />} />
+                </>
+              )}
+              <Route path="/reports" element={<div>ReportsPage</div>} />
+            </Route>
+            <Route path="/" element={<div>PublicRoute</div>}>
+              <Route path="/login" element={<div>LoginPage</div>} />
+              <Route path="/register" element={<div>RegiserPage</div>} />
+              <Route path="*" element={<Navigate to="/login" />} />
+            </Route>
+
+            <Route path="*" element={<div>ThereIsNoSuchPage</div>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './components/App';
 import { Provider } from 'react-redux';
-import { store } from 'redux/store';
-// import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from 'redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
-ReactDOM.createRoot(document.querySelector('#root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <PersistGate persistor={persistor}> */}
-      <App />
-      {/* </PersistGate> */}
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
