c131512b5f83178ba7ea85e7c5ded747fce8bdfc
efirnet <efirnet@gmail.com>
2023-01-28 15:52:48 +0200

    add privat route

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -2,7 +2,9 @@ import { useSelector } from 'react-redux';
 import { Navigate, Outlet } from 'react-router-dom';
 import { selectToken } from '../../redux/selectors';
 
-export const PrivateRoute = () => {
+const PrivateRoute = () => {
   const token = useSelector(selectToken);
   return token ? <Outlet /> : <Navigate to="/login" />;
 };
+
+export default PrivateRoute;
