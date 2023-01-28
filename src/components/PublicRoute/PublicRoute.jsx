70e605e77698b96ef8e6e8093ac6716aa3fd4205
efirnet <efirnet@gmail.com>
2023-01-28 15:01:09 +0200

    add public route

---
+++ src/components/PublicRoute/PublicRoute.jsx
@@ -0,0 +1,8 @@
+import { useSelector } from 'react-redux';
+import { Navigate, Outlet } from 'react-router-dom';
+import { selectToken } from '../../redux/selectors';
+
+export const PublicRoute = () => {
+  const token = useSelector(selectToken);
+  return token ? <Navigate to="/home/expenses" /> : <Outlet />;
+};
