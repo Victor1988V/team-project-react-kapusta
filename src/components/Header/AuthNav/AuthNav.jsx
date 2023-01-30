107ec46ad5a345034b8a4066bf69e813f35843ab
Anna Tsepilova <virgo.tsav@gmail.com>
2023-01-30 19:32:26 +0200

    make authNav user menu, refactored styled tags, routes

--- src/components/Header/AuthNav/AuthNav.jsx
+++ src/components/Header/AuthNav/AuthNav.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { selectIsLoggedIn, selectUserId } from 'redux/selectors';
+import { selectIsLoggedIn, selectUserEmail } from 'redux/selectors';
 import { logOut } from 'services/authAPI';
 
 import logoutImg from 'images/logout.svg';
@@ -9,19 +9,19 @@ import logoutImg from 'images/logout.svg';
 import LightModalWindow from 'components/ModalWindow/LightModalWindow/LightModalWindow';
 
 import {
-  StyledAuthNav,
-  StyledLoginLabel,
-  StyledLoginName,
-  StyledLogoutImg,
-  StyledVerticalLine,
-  StyledExitButton,
+  AuthNavContainer,
+  UserAvatar,
+  UserEmail,
+  LogoutImg,
+  VerticalLine,
+  ExitButton,
 } from 'components/Header/AuthNav/AuthNav.styled';
 
 export const AuthNav = () => {
   const [modalOpen, setModalOpen] = useState(false);
   const dispatch = useDispatch();
   const isLoggedIn = useSelector(selectIsLoggedIn);
-  const userEmail = useSelector(selectUserId);
+  const userEmail = useSelector(selectUserEmail);
 
   const handleClick = () => {
     dispatch(logOut());
@@ -36,27 +36,26 @@ export const AuthNav = () => {
   };
 
   return (
-    // isLoggedIn && (
-    <>
-      <StyledAuthNav>
-        {/* <StyledLoginLabel>{userEmail[0].toUpperCase()}</StyledLoginLabel> */}
-        {/* <StyledLoginName>{userEmail}</StyledLoginName> */}
-        <StyledLogoutImg
-          src={logoutImg}
-          alt="logout"
-          onClick={handleModalOpen}
-        />
-        <StyledVerticalLine></StyledVerticalLine>
-        <StyledExitButton type="button" onClick={handleModalOpen}>
-          Exit
-        </StyledExitButton>
-      </StyledAuthNav>
-      {modalOpen && (
-        <LightModalWindow closeModal={handleModalClose} dispatch={handleClick}>
-          Do you really want to leave?
-        </LightModalWindow>
-      )}
-    </>
-    // )
+    isLoggedIn && (
+      <>
+        <AuthNavContainer>
+          <UserAvatar>{userEmail[0].toUpperCase()}</UserAvatar>
+          <UserEmail>{userEmail}</UserEmail>
+          <LogoutImg src={logoutImg} alt="logout" onClick={handleModalOpen} />
+          <VerticalLine></VerticalLine>
+          <ExitButton type="button" onClick={handleModalOpen}>
+            Exit
+          </ExitButton>
+        </AuthNavContainer>
+        {modalOpen && (
+          <LightModalWindow
+            closeModal={handleModalClose}
+            dispatch={handleClick}
+          >
+            Do you really want to leave?
+          </LightModalWindow>
+        )}
+      </>
+    )
   );
 };
