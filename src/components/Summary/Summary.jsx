216d380936f25e87b90cd9317d871ee49cd13640
efirnet <efirnet@gmail.com>
2023-01-30 14:04:57 +0200

    edit components

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -1,7 +1,47 @@
-import { Div } from './Summary.styled';
+import { Table, Row, Month, TableTop, Value } from './Summary.styled';
+import monthNameTranslate from './SummaryMonthTranslate';
+import { useLocation } from 'react-router';
+import { useSelector } from 'react-redux';
+import {
+  selectIncomes,
+  selectExpenses,
+  selectIsLoading,
+} from '../../redux/selectors';
 
 const Summary = () => {
-  return <Div />;
+  const location = useLocation();
+  const isLoading = useSelector(selectIsLoading);
+  const incomeData = useSelector(selectIncomes);
+  const expensesData = useSelector(selectExpenses);
+
+  let data;
+
+  if (location.pathname === '/home/income') {
+    data = Object.entries(incomeData) ?? [];
+  }
+  if (location.pathname === '/home/expenses') {
+    data = Object.entries(expensesData) ?? [];
+  }
+
+  return (
+    !isLoading && (
+      <Table>
+        <TableTop>SUMMARY</TableTop>
+        {data?.map(item => {
+          if (item[1] === 'N/A') {
+            return false;
+          } else {
+            return (
+              <Row key={`${item[0]}${item[1]}`}>
+                <Month>{monthNameTranslate(item[0])}</Month>
+                <Value>{item[1]}.00</Value>
+              </Row>
+            );
+          }
+        })}
+      </Table>
+    )
+  );
 };
 
 export default Summary;
