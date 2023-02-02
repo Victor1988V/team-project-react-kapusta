3a1763a1f40fd09352c2297720f9da773c386b76
Victor1988V <110904832+Victor1988V@users.noreply.github.com>
2023-02-02 17:17:04 +0000

    Deploying to gh-pages from @ Victor1988V/team-project-react-kapusta@3fdd73122015db22f8e9a89fac301eb667dab263 🚀

---
+++ static/js/911.105001d9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkteam_project_react_kapusta=self.webpackChunkteam_project_react_kapusta||[]).push([[911],{6911:function(e,t,a){a.r(t),a.d(t,{default:function(){return u}});a(2791);var r=a(184);function u(){return(0,r.jsx)("div",{children:"There is no such page"})}}}]);
+//# sourceMappingURL=911.105001d9.chunk.js.map
